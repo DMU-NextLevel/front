5b7cabb2de1416b9d50893ec266e135f6856441b
limtjdghks <epaktldk0203@gmail.com>
2025-06-09 15:07:47 +0900

    feat: API 주소 환경변수 설정

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
+const baseUrl = process.env.REACT_APP_API_BASE_URL
+
 export const api = axios.create({
-    // baseURL: 'https://api.nextlevel.r-e.kr/',
-    baseURL:'http://localhost:8080/',
-    withCredentials: true
+	// baseURL: 'https://api.nextlevel.r-e.kr/',
+	baseURL: baseUrl,
+	withCredentials: true,
 })
 
 export const testApi = axios.create({
