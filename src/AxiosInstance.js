7924d3cebe67b71447d3ecd9ac6a143a110f0ca4
SeongHwan <epaktldk0203@gmail.com>
2025-06-09 15:14:21 +0900

    fix: AxiosInstance 수정

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -3,12 +3,6 @@ import axios from "axios"
 const baseUrl = process.env.REACT_APP_API_BASE_URL
 
 export const api = axios.create({
-	// baseURL: 'https://api.nextlevel.r-e.kr/',
 	baseURL: baseUrl,
 	withCredentials: true,
-})
-
-export const testApi = axios.create({
-    baseURL:'http://localhost:8080/',
-    withCredentials: true
 })
