04f19cba88eeae452560e2f1f456e23a33427181
송하성 <ssongsyj0203@gmail.com>
2025-09-16 23:39:21 +0900

    fix: 공지사항 상세보기 이미지 오류 수정 (#58)
    * refacter : 메인 리펙토리 초기작업
    
    * fix: 공지사항 상세 이미지 조회 오류 수정

--- src/pages/NoticeDetail.tsx
+++ src/pages/NoticeDetail.tsx
@@ -131,7 +131,7 @@ const NoticeDetail: React.FC = () => {
 
     imgTags.forEach((img, idx) => {
       if (article.imgs && article.imgs[idx]) {
-        img.setAttribute('src', `${api.defaults.baseURL}img/${article.imgs[idx]}`);
+        img.setAttribute('src', `${api.defaults.baseURL}/img/${article.imgs[idx]}`);
       }
     });
 
