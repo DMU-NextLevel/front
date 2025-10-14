e0cbfe964178f7cc117cfd5da1382f732403f13c
limtjdghks <epaktldk0203@gmail.com>
2025-10-14 22:19:01 +0900

    hotfix: 비로그인 에러 발생시 로그인 페이지로 리다이렉트

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -11,3 +11,21 @@ export const apiWithoutCredentials = axios.create({
 	baseURL: baseUrl,
 	withCredentials: false,
 })
+
+// Response interceptor - 401 에러 처리
+api.interceptors.response.use(
+	(response) => {
+		// 성공 응답은 그대로 반환
+		return response
+	},
+	(error) => {
+		// 401 Unauthorized 에러 처리
+		if (error.response && error.response.status === 401) {
+			console.log('인증 에러 발생: 로그인 페이지로 이동합니다.')
+			// 로그인 페이지로 리다이렉트
+			window.location.href = '/login'
+		}
+		// 다른 에러는 그대로 전달
+		return Promise.reject(error)
+	}
+)
